059a1547ba3617e7ea222e5981dccfc3bab4a055
RomanSuprun <roman.suprun@otakoyi.com>
2025-03-04 13:15:29 +0200

    fix: added log

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
+const databaseurl = process.env.DATABASE_URL
+
+console.log('///////////////',{databaseurl})
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
